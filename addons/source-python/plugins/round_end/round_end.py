aace0743e7334d0d043f40e23919097c41fc79de
srpg <44681214+srpg@users.noreply.github.com>
2021-07-19 11:21:30 +0300

    Add files via upload

---
+++ addons/source-python/plugins/round_end/round_end.py
@@ -0,0 +1,33 @@
+from events import Event
+from entities.entity import Entity
+from listeners.tick import Delay
+from messages import HudDestination, TextMsg
+from cvars import cvar
+
+can_delay = True
+
+@Event('round_start')
+def round_start(args):
+	global can_delay
+	global mydelay
+	ftime = cvar.find_var('mp_freezetime').get_int()
+	rtime = cvar.find_var('mp_roundtime').get_int()
+	rtime = float(rtime * 60.0)
+	rtime = float(rtime + ftime)
+	rtime += 5
+	if can_delay:
+		mydelay = Delay(float(rtime), endRound)
+		print('[Round End]: Timer have been started!')
+		can_delay = False
+	else:
+		mydelay.cancel()
+		print('[Round End]: Old timer have cancelled!!')
+		mydelay = Delay(float(rtime), endRound)
+		print('[Round End]: Started new timer!')
+
+def endRound():
+	global can_delay
+	Entity.find_or_create('info_map_parameters').fire_win_condition(9)
+	TextMsg('ROUND DRAW', HudDestination.CENTER).send()
+	can_delay = True
+	print('[Round End]: Round have been ended!')
