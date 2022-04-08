a6ef6cafbcd5a9c175f1405299ccf07380855f92
srpg <44681214+srpg@users.noreply.github.com>
2022-04-08 18:27:12 +0300

    Added bomb site and hostage rescue checker
    Added checker for the map bombsite/hostage rescue zone.
    If it doesn't find bombsite/rescue zone, automatic round ender get triggered

--- addons/source-python/plugins/round_end/round_end.py
+++ addons/source-python/plugins/round_end/round_end.py
@@ -1,26 +1,34 @@
-from events import Event
-from entities.entity import Entity
-from listeners.tick import Delay
-from messages import HudDestination, TextMsg
 from cvars import cvar
+from events import Event
+from messages import TextMsg
+from listeners.tick import Delay
+from entities.entity import Entity
+from filters.entities import EntityIter
 
 can_delay = True
 
+def bomb_amounts():
+	return len(EntityIter('func_bomb_target'))
+
+def hostage_amounts():   
+	return len(EntityIter('func_hostage_rescue'))
+
 @Event('round_start')
 def round_start(args):
-	global can_delay
-	global mydelay
-	ftime = cvar.find_var('mp_freezetime').get_float()
-	rtime = cvar.find_var('mp_roundtime').get_float() * 60.0 + ftime + 5.0
-	if can_delay:
-		mydelay = Delay(float(rtime), endRound)
-		print('[Round End]: Timer have been started!')
-		can_delay = False
-	else:
-		mydelay.cancel()
-		print('[Round End]: Old timer have cancelled!!')
-		mydelay = Delay(float(rtime), endRound)
-		print('[Round End]: Started new timer!')
+	if bomb_amounts() == 0 or hostage_amounts() == 0 or hostage_amounts() == 0 and bomb_amounts() == 0:
+		global can_delay
+		global mydelay
+		ftime = cvar.find_var('mp_freezetime').get_float()
+		rtime = cvar.find_var('mp_roundtime').get_float() * 60.0 + ftime + 5.0
+		if can_delay:
+			mydelay = Delay(float(rtime), endRound)
+			print('[Round End]: Timer have been started!')
+			can_delay = False
+		else:
+			mydelay.cancel()
+			print('[Round End]: Old timer have cancelled!!')
+			mydelay = Delay(float(rtime), endRound)
+			print('[Round End]: Started new timer!')
 
 def endRound():
 	global can_delay
